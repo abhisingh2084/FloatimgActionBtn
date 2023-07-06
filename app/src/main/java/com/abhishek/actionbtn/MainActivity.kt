eb8e95f953bc0c8c7bb841e26400dbdf2db95482
NXT Design <developer@nxtdesigns.com>
2023-07-07 03:50:36 +0530

    initial commit

---
+++ app/src/main/java/com/abhishek/actionbtn/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.abhishek.actionbtn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.abhishek.actionbtn.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private var textList = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        addToList()
+        setUpRecyclerView()
+        setUpFab()
+
+    }
+
+    private fun setUpFab() {
+        val fab = binding.fab
+
+        fab.setOnClickListener {
+            Toast.makeText(applicationContext, "New chat clicked*", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.rvRecycler.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0) {
+                    fab.collapse()
+                } else {
+                    fab.expand()
+                }
+            }
+        })
+    }
+
+    private fun setUpRecyclerView() {
+
+        binding.rvRecycler.layoutManager = LinearLayoutManager(applicationContext)
+        binding.rvRecycler.adapter = RecyclerAdapter(textList)
+    }
+
+    private fun addToList() {
+
+        for (i in 0..100) {
+            textList.add("Sample text $i")
+        }
+    }
+}
+
+
